948da5ad2bc1010a2f2033d9ed962b8a669f1fe3
Nithin S Varrier <nithin486@hotmail.com>
2024-08-28 17:59:12 +0000

    fix: calendar - refresh token not setup

--- auth.js
+++ auth.js
@@ -1,22 +1,25 @@
 const passport = require("passport");
 const MicrosoftGraphStrategy = require("passport-microsoft").Strategy;
-require("dotenv").config();
+const refresh = require('passport-oauth2-refresh');
 
+require("dotenv").config();
+const strategy = new MicrosoftGraphStrategy(
+  {
+    clientID: process.env.MICROSOFT_APP_ID,
+    clientSecret: process.env.MICROSOFT_APP_SECRET,
+    callbackURL: process.env.MICROSOFT_REDIRECT_URI,
+    scope: ["User.Read", "Calendars.ReadWrite"],
+  },
+  function (accessToken, refreshToken, profile, done) {
+    // User profile is available in `profile` object
+    return done(null, {accessToken, refreshToken ,...profile});
+  },
+);
 passport.use(
   "microsoft",
-  new MicrosoftGraphStrategy(
-    {
-      clientID: process.env.MICROSOFT_APP_ID,
-      clientSecret: process.env.MICROSOFT_APP_SECRET,
-      callbackURL: process.env.MICROSOFT_REDIRECT_URI,
-      scope: ["User.Read", "Calendars.ReadWrite"],
-    },
-    function (accessToken, refreshToken, profile, done) {
-      // User profile is available in `profile` object
-      return done(null, profile);
-    },
-  ),
+  strategy
 );
+refresh.use(strategy);
 
 passport.serializeUser(function (user, done) {
   done(null, user);
